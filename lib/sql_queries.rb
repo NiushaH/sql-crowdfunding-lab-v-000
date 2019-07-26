d12e551522645d5db403e24fc1b25049515c693d
Niusha H <niusha.hadziomerovic@gmail.com>
2019-07-26 15:08:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
-ON pledges.amount >= projects.funding_goal"
+ON pledges.amount <= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
