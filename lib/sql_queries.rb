ec7232c690b2c5f7ee24d0cf68c8312be8a9e940
Niusha H <niusha.hadziomerovic@gmail.com>
2019-07-26 15:22:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,7 +53,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT *
-  FROM music
+  FROM projects.category = "music"
   INNER JOIN 
   ON 
   GROUP BY
